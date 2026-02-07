3d6d832f8555df35dcbdbcd83687206dc1172ac7
Mikael Lirbank <mikael@lirbank.com>
2026-02-07 13:15:36 -0800

    feat: integrate storage service into command handling

--- src/backend/services.ts
+++ src/backend/services.ts
@@ -1,13 +1,17 @@
-// import { homedir } from 'os';
+import { homedir } from 'os';
 import { createLogger } from './core/logger';
 import { createRunner } from './core/runner';
+import { createStorage } from './core/storage';
 
 export const buildServices = ({ logLevels }: { logLevels: ('info' | 'debug')[] }) => {
-    // const homeDir = homedir();
+    const homeDir = homedir();
     const logger = createLogger('bgit', logLevels);
     const runner = createRunner();
+    const storage = createStorage({ logger, homeDir });
+
     return {
         logger,
         runner,
+        storage,
     };
 };
